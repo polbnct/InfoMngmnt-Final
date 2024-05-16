91e38fb450f73ce24709978bf02e84bfcf6a5a7e
vincentypauly <112044566+polbnct@users.noreply.github.com>
2024-05-16 13:10:41 +0800

    Update handleforms.php

--- handleforms.php
+++ handleforms.php
@@ -10,22 +10,7 @@ if(isset($_POST['submitBtn'])) {
 	$description = $_POST['description']; 
 
 	makeATask($conn, $title, $description); 
-}
-?>
-<?php  
-require_once('dbConfig.php'); 
 
-require_once('functions.php'); 
-
-if(isset($_POST['submitBtn'])) { 
-
-	$title = $_POST['title']; 
-
-	$description = $_POST['title']; 
-
-	makeATask($conn, $title, $description); 
-
-	
 	header('Location: index.php');
 }
 ?>
